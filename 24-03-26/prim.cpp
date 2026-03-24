4d011df6603c31542e2ff221c533f64be7af2ad0
Monaal Sharma <157833279+sleeping-anonymous@users.noreply.github.com>
2026-03-24 12:40:40 +0530

    24-03

---
+++ 24-03-26/prim.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> mat = {
+        {5, 4, 9}, {5, 1, 4}, {1, 2, 2}, {1, 4, 1}, {4, 2, 3}, {4, 3, 5}, {2, 6, 7}, {2, 3, 3}, {3, 6, 8}};
+
+    int n = 6;
+
+    vector<vector<pair<int, int>>> adj(n + 1);
+
+    // build graph
+    for (auto &m : mat)
+    {
+        int u = m[0];
+        int v = m[1];
+        int w = m[2];
+
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    pq.push({0, 1});
+
+    vector<int> visited(n + 1, 0);
+    int ans = 0;
+
+    while (!pq.empty())
+    {
+        int wt = pq.top().first;
+        int node = pq.top().second;
+
+        pq.pop();
+
+        if (visited[node])
+            continue;
+
+        visited[node] = 1;
+        ans += wt;
+
+        for (auto nei : adj[node])
+        {
+            if (!visited[nei.first])
+            {
+                pq.push({nei.second, nei.first});
+            }
+        }
+    }
+
+    cout << ans << " ";
+
+    return 0;
+}
